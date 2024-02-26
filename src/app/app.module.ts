5b71f51379e75b18d251baceec66fa812c9da538
kphutiyagae <kopanophutiyagae@gmail.com>
2024-02-26 12:52:37 +0200

    feat(none): :sparkles: Added Home, Budget and Analytics Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BudgetComponent } from './pages/budget/budget.component';
+import { AnalyticsComponent } from './pages/analytics/analytics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BudgetComponent,
+    AnalyticsComponent
   ],
   imports: [
     BrowserModule,
