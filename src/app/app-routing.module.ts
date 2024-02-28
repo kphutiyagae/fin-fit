11502ba356be59519a421859ddfc0758f695857a
kphutiyagae <kopanophutiyagae@gmail.com>
2024-02-28 08:39:57 +0200

    feat(none): :sparkles: Added Tailwind and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AnalyticsComponent} from "./pages/analytics/analytics.component";
+import {BudgetComponent} from "./pages/budget/budget.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {SignupComponent} from "./pages/signup/signup.component";
+import {HomeComponent} from "./pages/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'analytics',
+    component: AnalyticsComponent
+  },
+  {
+    path: 'budget',
+    component: BudgetComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
