40a43b65e511e4aceb1a66adc94be07ae89170b0
kphutiyagae <kopanophutiyagae@gmail.com>
2024-02-29 11:19:13 +0200

    feat(none): :construction: WIP: Added Budget interface

---
+++ src/app/models/IBudget.ts
@@ -0,0 +1,11 @@
+import {ITransaction} from "./ITransaction";
+
+export interface IBudget {
+  id: string;
+  title: string;
+  totalExpense: number;
+  budgetLimit: number;
+  transactions?: ITransaction[];
+  dateStart?: string;
+  dateEnd?:string;
+}
