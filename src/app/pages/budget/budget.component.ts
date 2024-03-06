8203bc390988309f600b62cd8aff90e0aa48c04a
kphutiyagae <kopanophutiyagae@gmail.com>
2024-03-06 11:45:20 +0200

    feat(none): :construction: WIP: Added template rendering to page

--- src/app/pages/budget/budget.component.ts
+++ src/app/pages/budget/budget.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {concat, filter, map, merge, Observable, switchMap, tap} from "rxjs";
+import {IBudget} from "../../models/IBudget";
+import {Store} from "@ngxs/store";
+import {ActivatedRoute, UrlSegment} from "@angular/router";
+import {BudgetState} from "../../store/budget/state/BudgetState";
 
 @Component({
   selector: 'app-budget',
@@ -6,5 +11,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./budget.component.scss']
 })
 export class BudgetComponent {
+
+  currentBudget$: Observable<IBudget | undefined>;
+
+  constructor(private store: Store, private route: ActivatedRoute) {
+    this.currentBudget$ = this.route.url
+      .pipe(
+        map( urlSegments => {
+          if(urlSegments.at(1)) return urlSegments?.at(1)?.toString() ?? '';
+          return urlSegments?.at(0)?.toString() ?? '';
+        }),
+        switchMap(budgetId => {
+           return this.store.select(BudgetState.getBudgets).pipe(
+             map(budgetList => budgetList.find(budget => budget.id === budgetId))
+           )
+        }),
+        tap(console.log)
+      )
+
+  }
 
 }
