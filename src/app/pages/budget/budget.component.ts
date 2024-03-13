833d8f5894e36e68e2d52f7b3f0b5f002cb3f152
kphutiyagae <kopanophutiyagae@gmail.com>
2024-03-13 11:36:52 +0200

    feat(none): :construction: WIP: Added editBudget modal.

--- src/app/pages/budget/budget.component.ts
+++ src/app/pages/budget/budget.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {concat, filter, map, merge, Observable, switchMap, tap} from "rxjs";
 import {IBudget} from "../../models/IBudget";
 import {Store} from "@ngxs/store";
 import {ActivatedRoute, Router, UrlSegment} from "@angular/router";
 import {BudgetState} from "../../store/budget/state/BudgetState";
 import {getRemainingDays} from "../../utils/utils";
+import {IonModal} from "@ionic/angular";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
@@ -15,6 +17,12 @@ export class BudgetComponent {
   currentBudget$: Observable<IBudget | undefined>;
 
   calculateRemainingDays = getRemainingDays;
+
+  @ViewChild(IonModal) addTransactionModal!: IonModal;
+  @ViewChild(IonModal) editBudgetModal!: IonModal;
+
+  public addNewTransactionForm: FormGroup;
+  public editBudgetForm!: FormGroup;
 
   navigateToHome(){
     this.router.navigate([`/`]);
@@ -29,12 +37,32 @@ export class BudgetComponent {
         }),
         switchMap(budgetId => {
            return this.store.select(BudgetState.getBudgets).pipe(
-             map(budgetList => budgetList.find(budget => budget.id === budgetId))
+             map(budgetList => {
+               const budget = budgetList.find(budget => budget.id === budgetId);
+
+               if(budget){
+                 this.editBudgetForm = new FormGroup({
+                   budgetTitle: new FormControl(budget.title, [Validators.required, Validators.minLength(1)]),
+                   budgetTotalExpense: new FormControl(budget.totalExpense, [Validators.required, Validators.min(0)]),
+                   budgetDateStart: new FormControl(budget.dateStart, [Validators.required]),
+                   budgetEndDate: new FormControl(budget.dateEnd),
+                   budgetTransactions: new FormControl(budget.transactions)
+                 })
+               }
+
+               return budget;
+             })
            )
         }),
         tap(console.log)
       )
 
+    this.addNewTransactionForm = new FormGroup({
+      transactionTitle: new FormControl('', [Validators.required, Validators.minLength(1)]),
+      transactionAmount: new FormControl(0),
+      transactionDate: new FormControl('', [Validators.required]),
+      transactionCategory: new FormControl('', [Validators.required])
+    });
   }
 
 }
